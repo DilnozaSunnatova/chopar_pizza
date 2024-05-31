a641fdbfaf63254bc6e717193b5f2ce5f2a5f0e6
DilnozaSunnatova <dilnozasunnatova81@gmail.com>
2024-05-31 12:44:37 +0500

    nimadir

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_up = models.DateTimeField()
+    updated_up = models.DateTimeField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class Extra(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='extra')
+    price = models.DecimalField(max_digits=10, decimal_places= 2)
+
+class Size(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=10, decimal_places= 2)
+
+class Product(BaseModel):
+    name = models.CharField(max_length= 30)
+    image = models.ImageField(upload_to='product')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places= 2)
+    size = models.CharField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size,null=True, on_delete=models.CASCADE)
+    extra = models.ForeignKey(Extra, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
