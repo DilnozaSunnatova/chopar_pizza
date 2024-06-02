d0c4df03630d48700bcecbcd668e1549730b1406
abbosbekdevid <lordmrboss@gmail.com>
2024-06-02 16:39:23 -0500

    pushed abs

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import AuthenticationRegister
+from .views import AuthenticationRegister, ContactListAPIView
 
 urlpatterns=[
     path('register/', AuthenticationRegister.as_view()),
-    
+    path('contact/', AuthenticationRegister.as_view()),
     
 ]
